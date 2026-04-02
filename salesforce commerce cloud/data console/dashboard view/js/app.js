9acfe99b47580a98f1ad17341609a5fe33dada9d
madhav100 <diveshtotoo@gmail.com>
2026-04-02 15:00:06 -0400

    added data console
    added data console

--- salesforce commerce cloud/data console/dashboard view/js/app.js
+++
@@ -1,5 +0,0 @@
-import { pipelinePlugin } from './plugins/pipelinePlugin.js';
-
-const app = document.getElementById('app');
-const plugins = [pipelinePlugin];
-plugins.forEach((plugin) => plugin(app));
